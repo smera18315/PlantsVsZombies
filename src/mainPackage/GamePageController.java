bda6c37cff947603d16c6abe89ffa81986bd228a
smera18315 <56244678+smera18315@users.noreply.github.com>
2019-11-01 22:28:15 +0530

    v 2.0

---
+++ src/mainPackage/GamePageController.java
@@ -0,0 +1,7 @@
+package mainPackage;
+
+import javafx.fxml.FXML;
+
+public class GamePageController {
+
+}
--- src/mainPackage/MainPageController.java
+++ src/mainPackage/MainPageController.java
@@ -6,9 +6,59 @@ import javafx.scene.image.*;
 
 public class MainPageController {
 	@FXML
-    public void hover(MouseEvent e){
+    public void mouseEnterStartGame(MouseEvent e){
         ImageView m = (ImageView) e.getSource();
         m.setImage(new Image("file:Images/StartGameGlow.png"));
 
     }
+	
+	@FXML
+    public void mouseExitStartGame(MouseEvent e){
+        ImageView m = (ImageView) e.getSource();
+        m.setImage(new Image("file:Images/StartGame.png"));
+
+    }
+	
+	@FXML
+    public void mouseEnterLoadGame(MouseEvent e){
+        ImageView m = (ImageView) e.getSource();
+        m.setImage(new Image("file:Images/LoadGameGlow.png"));
+
+    }
+	
+	@FXML
+    public void mouseExitLoadGame(MouseEvent e){
+        ImageView m = (ImageView) e.getSource();
+        m.setImage(new Image("file:Images/LoadGame.png"));
+
+    }
+	
+	@FXML
+    public void mouseEnterExitGame(MouseEvent e){
+        ImageView m = (ImageView) e.getSource();
+        m.setImage(new Image("file:Images/ExitGameGlow.png"));
+
+    }
+	
+	@FXML
+    public void mouseExitExitGame(MouseEvent e){
+        ImageView m = (ImageView) e.getSource();
+        m.setImage(new Image("file:Images/ExitGame.png"));
+
+    }
+	
+	@FXML
+    public void mouseEnterChooseLevel(MouseEvent e){
+        ImageView m = (ImageView) e.getSource();
+        m.setImage(new Image("file:Images/ChooseLevelGlow.png"));
+
+    }
+	
+	@FXML
+    public void mouseExitChooseLevel(MouseEvent e){
+        ImageView m = (ImageView) e.getSource();
+        m.setImage(new Image("file:Images/ChooseLevel.png"));
+
+    }
 }
+
